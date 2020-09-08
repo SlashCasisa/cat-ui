74e7968d123b72cc36f212e7944f0c39bbf8c2e8
unknown <836513735@qq.com>
2020-09-08 09:30:57 +0800

    switch组件

--- src/lib/Switch.vue
+++ src/lib/Switch.vue
@@ -1,6 +1,24 @@
 <template>
-<button><span></span></button>
+<button :class="{checked}" @click="toggle"><span></span></button>
 </template>
+
+<script>
+import {
+    ref
+} from 'vue'
+export default {
+    setup() {
+        const checked = ref(false)
+        const toggle = () => {
+            checked.value = !checked.value
+        }
+        return {
+            checked,
+            toggle
+        }
+    }
+}
+</script>
 
 <style lang="scss" scoped>
 $h: 22px;
@@ -10,7 +28,7 @@ button {
     height: $h;
     width: $h*2;
     border: none;
-    background: blue;
+    background: #ccc;
     border-radius: $h/2;
     position: relative;
 }
@@ -18,10 +36,18 @@ button {
 span {
     position: absolute;
     top: 2px;
-    left: calc(100% - #{h2} -2px);
+    left: 2px;
     height: $h2;
     width: $h2;
     background: #fff;
     border-radius: $h2/2;
+}
+
+button.checked {
+    background: blue;
+}
+
+button.checked>span {
+    left: calc(100% - 20px);
 }
 </style>
