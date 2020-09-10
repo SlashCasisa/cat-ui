127104400a723e6ff780e887187ea1f7dc31ad83
unknown <836513735@qq.com>
2020-09-10 16:19:41 +0800

    dialog 动态弹出框

--- src/components/DialogDemo.vue
+++ src/components/DialogDemo.vue
@@ -1,19 +1,27 @@
 <template>
-<div style="position:relative;z-index:1">
-    <Button @click="toggle">toggle</Button>
-    <Dialog v-model:visible="x" :closeOnClickOverlay="false" :ok="f1" :cancel="f2" title="netlify">
-        <template v-slot:content> <strong>nice to meet you</strong></template>
-        <template v-slot:title> <strong>netlify</strong></template>
-    </Dialog>
-</div>
+<!-- <div style="position:relative;z-index:1"> -->
+<div>Dialog 示例</div>
+<h1>示例1</h1>
+<Button @click="toggle">toggle</Button>
+<Dialog v-model:visible="x" :closeOnClickOverlay="false" :ok="f1" :cancel="f2" title="netlify">
+    <template v-slot:content> <strong>nice to meet you</strong></template>
+    <template v-slot:title> <strong>netlify</strong></template>
+</Dialog>
+<!--</div>-->
+<h1>示例2</h1>
+<Button @click="showDialog">show</Button>
 </template>
 
 <script lang="ts">
 import Dialog from '../lib/Dialog.vue'
 import Button from '../lib/Button.vue'
 import {
-    ref
+    ref,
+    h
 } from 'vue'
+import {
+    openDialog
+} from '../lib/openDialog'
 export default {
     components: {
         Dialog,
@@ -30,11 +38,24 @@ export default {
             // return false
         }
         const f2 = () => {}
+        const showDialog = () => {
+            openDialog({
+                title: h('strong', {}, '标题'),
+                content: '你好',
+                ok() {
+                    console.log('ok')
+                },
+                cancel() {
+                    console.log('cancel')
+                }
+            })
+        }
         return {
             x,
             toggle,
             f1,
-            f2
+            f2,
+            showDialog
         }
     }
 }
