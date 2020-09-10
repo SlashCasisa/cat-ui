ac6d281238ce0219fe68efa018534cd98a6f681a
unknown <836513735@qq.com>
2020-09-10 09:52:44 +0800

    dialog组件

--- src/components/DialogDemo.vue
+++ src/components/DialogDemo.vue
@@ -1,9 +1,15 @@
 <template>
-<div>dialogdemo</div>
+<div>
+    <Dialog />
+</div>
 </template>
 
 <script lang="ts">
+import Dialog from '../lib/Dialog.vue'
 export default {
+    components: {
+        Dialog
+    },
     setup() {
         console.log('enter dialog')
     }
