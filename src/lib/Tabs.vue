c62e41d2c2e3942bb7181c5be81ec7359b98d98a
unknown <836513735@qq.com>
2020-09-11 10:34:38 +0800

    tabs确认子组件

--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -1,3 +1,27 @@
 <template>
-<div>Tabs</div>
+<div>Tabs
+    <component :is="defaults[0]" />
+    <component :is="defaults[1]" />
+</div>
 </template>
+
+<script lang="ts">
+import Tab from './Tab.vue'
+export default {
+    setup(props, context) {
+        // console.log({
+        //     ...context.slots.default()
+        // }, 'context')
+        const defaults = context.slots.default()
+        // console.log(defaults[1].type)
+        defaults.forEach((tag) => {
+            if (tag.type !== Tab) {
+                throw new Error('Tabs必须是Tab')
+            }
+        })
+        return {
+            defaults
+        }
+    }
+}
+</script>
