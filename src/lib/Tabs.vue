cdb17428a472ee764c33dd139f15757f4f123fd5
unknown <836513735@qq.com>
2020-09-11 17:13:59 +0800

    修复tags的切换

--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -15,7 +15,7 @@ import Tab from './Tab.vue'
 import {
     ref,
     computed,
-    // onMounted,
+    onMounted,
     // onUpdated,
     watchEffect
 } from 'vue'
@@ -50,19 +50,21 @@ export default {
         // }
         // onMounted(x)
         // onUpdated(x)
-        watchEffect(() => {
-            const {
-                width
-            } = selectedItem.value.getBoundingClientRect()
-            indicator.value.style.width = width + 'px'
-            const {
-                left: left1
-            } = container.value.getBoundingClientRect()
-            const {
-                left: left2
-            } = selectedItem.value.getBoundingClientRect()
-            const left = left2 - left1
-            indicator.value.style.left = left + 'px'
+        onMounted(() => {
+            watchEffect(() => {
+                const {
+                    width
+                } = selectedItem.value.getBoundingClientRect()
+                indicator.value.style.width = width + 'px'
+                const {
+                    left: left1
+                } = container.value.getBoundingClientRect()
+                const {
+                    left: left2
+                } = selectedItem.value.getBoundingClientRect()
+                const left = left2 - left1
+                indicator.value.style.left = left + 'px'
+            })
         })
         // console.log({
         //     ...context.slots.default()
